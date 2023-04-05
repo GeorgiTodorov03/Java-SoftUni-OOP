b9fc7dc0eb6e5fda98627737196db8be0a4e74ae
Georgi Todorov <106263573+GeorgiTodorov03@users.noreply.github.com>
2023-04-05 21:57:12 +0300

    Add files via upload

---
+++ UnitTesting/src/test/java/rpg_lab/HeroTest.java
@@ -0,0 +1,19 @@
+package rpg_lab;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HeroTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() {
+
+
+    }
+}
